53c3767c927262b6e361a3a866332947a1281d80
Kyle Stevens <43458322+kyle-stevens@users.noreply.github.com>
2022-08-23 17:11:01 -0700

    Messing around with lighting

--- Rogue_Game/PlayerCharacter.gd
+++ Rogue_Game/PlayerCharacter.gd
@@ -7,19 +7,44 @@ export (int) var gravity = 1200
 var velocity = Vector2()
 var jumping = false
 
-func get_input():
-	velocity.x = 0
-	var right = Input.is_action_pressed('ui_right')
-	var left = Input.is_action_pressed('ui_left')
-	var jump = Input.is_action_just_pressed('ui_select')
+export (float, 0, 1.0) var friction = 0.1
+export (float, 0, 1.0) var acceleration = 0.25
 
-	if jump and is_on_floor():
+var attack_reset = true
+var timer
+
+func _ready():
+	timer = Timer.new()
+	timer.connect("timeout",self,"_on_timer_timeout") 
+	timer.set_wait_time(3) # this is based off equiped weapon
+	add_child(timer) #to process
+	timer.start() #to start
+
+func get_input():
+	### Movement Code
+	var dir = 0
+	if Input.is_action_pressed("ui_right"):
+		dir += 1
+	if Input.is_action_pressed("ui_left"):
+		dir -= 1
+	if dir != 0:
+		velocity.x = lerp(velocity.x, dir * run_speed, acceleration)
+	else:
+		velocity.x = lerp(velocity.x, 0, friction)
+		
+	if Input.is_action_just_pressed("ui_select") and is_on_floor():
 		jumping = true
 		velocity.y = jump_speed
-	if right:
-		velocity.x += run_speed
-	if left:
-		velocity.x -= run_speed
+		
+	### Combat Code (not yet implemented
+	if Input.is_action_pressed("mouse_left") and attack_reset:
+		print("attack")
+		attack_reset = false
+		timer.start()
+		# will initiate attack animation and spawn hitbox, use a timer for delay
+		#both melee strike and ranged, use same attack mouse point follower
+		#melee determines where it stabs versus where it shoots
+		#three strike combos
 
 func _physics_process(delta):
 	get_input()
@@ -27,3 +52,6 @@ func _physics_process(delta):
 	if jumping and is_on_floor():
 		jumping = false
 	velocity = move_and_slide(velocity, Vector2(0, -1))
+
+func _on_timer_timeout():
+   attack_reset = true
